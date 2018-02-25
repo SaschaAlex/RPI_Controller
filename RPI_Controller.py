c901d7964ec4319f333f78a2593f766a334a38f8
Alex-Antoine Caron <alex.antoine.caron@hotmail.com>
2018-02-24 20:37:39 -0500

    Initial commit

---
+++ RPI_Controller.py
@@ -0,0 +1,35 @@
+#This program has been create by Alex-Antoine Caron (2018-02-24)
+#Feel free to modify the code
+#This program required a Raspberry pi for working and a NPN transistor
+#otherwise you won't be able to use it.
+#PS:This program has been used with a Rasberry pi 3B
+# and I use a PN2222A NPN transistor
+
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(True)
+GPIO.setmode(GPIO.BOARD)
+
+
+GPIO.setup(11,GPIO.OUT)  # Currant output 3.3V
+GPIO.output(11,0) # Make sure the Pin is off 
+
+
+print("Welcome!, You can press CTRL+C for exit the program!")
+
+try:
+    while(True):
+        request = input("press : (1 = on , 0 = off): ")
+        if(len(request) ==1):
+            GPIO.output(11,int(request[0]))
+            if(int(request[0]) == 0):
+                print("GPIO 11 is off!")
+            else:
+                if(int(request[0]) == 1):
+                    print("GPIO 11 is on!")
+                else:
+                    GPIO.output(11,0)
+                    print("invalid input!")
+finally:
+    GPIO.output(11,0)
+    GPIO.cleanup()
