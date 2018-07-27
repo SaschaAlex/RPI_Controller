504adf198dc13b7040b883d0f5d4c54de49f5a92
Alex-Antoine Caron <36684458+SaschaAlex@users.noreply.github.com>
2018-07-26 20:11:46 -0400

    Update RPI_Controller.py

--- RPI_Controller.py
+++ RPI_Controller.py
@@ -1,9 +1,3 @@
-#This program has been create by Alex-Antoine Caron (2018-02-24)
-#Feel free to modify the code
-#This program required a Raspberry pi for working and a NPN transistor
-#otherwise you won't be able to use it.
-#PS:This program has been used with a Rasberry pi 3B
-# and I use a PN2222A NPN transistor
 
 import RPi.GPIO as GPIO
 
